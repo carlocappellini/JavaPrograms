5278c6415d8829cb7387dbc85aea6dd184390321
Bolimbo <carlo.cappellini@yahoo.com>
2018-06-12 21:13:58 -0500

    Statements White Spacing And Indenting

---
+++ src/StatementsWhiteSpacingAndIndenting.java
@@ -0,0 +1,21 @@
+public class StatementsWhiteSpacingAndIndenting {
+    public static void main(String[] args) {
+
+        //statement:
+        int myvariable = 50;
+        myvariable++;
+        myvariable--;
+
+        System.out.println("this a test ");
+        //        System.out.println("this a statement ");
+
+
+        System.out.println("this is" +
+                "another" +
+                "still more");
+
+        int anotherVariable = 50;myvariable--;System.out.println("this is another one");
+
+    }
+}
+
