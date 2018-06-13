5a2af801853ac135d759c50a5964c8bb79158019
Bolimbo <carlo.cappellini@yahoo.com>
2018-06-12 21:09:54 -0500

    Positive Negative Or Zero

---
+++ src/PositiveNegativeOrZero.java
@@ -0,0 +1,21 @@
+public class PositiveNegativeOrZero {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-59);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number){
+
+        if (number > 0) {
+            System.out.println("positive");
+
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if(number == 0){
+            System.out.println(" The number was zero");
+        }
+    }
+
+}
