ae9fc27abcf26f411dcc12ef1d57f241ca6a6b6d
Bolimbo <carlo.cappellini@yahoo.com>
2018-06-12 21:11:54 -0500

    If Keyword And Code BLocks

---
+++ src/IfKeyWordAndCodeBlocks.java
@@ -0,0 +1,49 @@
+public class IfKeyWordAndCodeBlocks {
+
+
+        public static void main(String[] args) {
+
+
+            boolean gameOver = true;
+            int score = 1000;
+            int levelCompleted = 5;
+            int bonus = 100;
+
+
+//        //use code blocks '{ }' for clearer code
+//        if (score < 5000 && score > 1000) {
+//            System.out.println("Your score was less than 5000 but                     greater than 1000");
+//        } else if (score < 1000) {
+//            System.out.println("Your score was less than 1000");
+//        } else {
+//            System.out.println("Got here ");
+//        }
+
+            if (gameOver){
+                int finalScore = score +(levelCompleted + bonus ) ;
+                finalScore += 1000;
+                System.out.println("Your final score was: " + finalScore);
+            }
+
+//
+//        boolean newGameOver = true;
+//        int newScore = 10000;
+//        int newLevelCompleted = 8;
+//        int newBonus = 200;
+
+
+            gameOver = true;
+            score = 10000;
+            levelCompleted = 8;
+            bonus = 200;
+
+            if (gameOver) {
+                int secondScore = score + (levelCompleted * bonus);
+                secondScore+= 1000;
+                System.out.println(secondScore);
+            }
+        }
+
+
+    }
+
