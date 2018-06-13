519d647e696a90ec13ef3eb13727056eaec07a9a
Bolimbo <carlo.cappellini@yahoo.com>
2018-06-12 21:08:01 -0500

    Mega Bytes Converter

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,27 @@
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+
+        printMegaBytesAndKiloBytes(4506);
+
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if (kiloBytes <= 0) {
+
+            System.out.println("Invalid Values");
+        } else {
+
+
+            int megaByte = 1024;
+
+            int megaBytes = kiloBytes / megaByte;
+
+            int remainder = megaBytes % kiloBytes;
+
+
+            System.out.println(kiloBytes + " KB " + " = " + megaBytes + " MB " + " and " + remainder + " KB ");
+        }
+    }
+
+}
