fa01fdc828b6770fba1d1054aa9cd955f47ec62d
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-29 22:11:01 -0500

    created private variable of type Resolution -resolution will be a class

--- src/com/carlocappellini/Monitor.java
+++ src/com/carlocappellini/Monitor.java
@@ -1,4 +1,15 @@
 package com.carlocappellini;
 
 public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolution;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
 }
