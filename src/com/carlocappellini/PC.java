02158a2ba49bdea43f147eb962d17510a2a40afa
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-29 22:33:08 -0500

    variables declared in PC are variables holding objects in other classes

--- src/com/carlocappellini/PC.java
+++ src/com/carlocappellini/PC.java
@@ -2,4 +2,30 @@ package com.carlocappellini;
 
 public class PC {
 
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherBoard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherBoard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherBoard = motherBoard;
+    }
+
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherBoard() {
+        return motherBoard;
+    }
+
+
+
+
 }
