4b77152bfda3132130dfde97a54a15e0b5b7c1ba
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-30 12:14:21 -0500

    PC private getMethods wont  be accessible in the main class

--- src/com/carlocappellini/PC.java
+++ src/com/carlocappellini/PC.java
@@ -31,18 +31,7 @@ public class PC {
 
     public static void main(String[] args) {
 
-        Dimensions dimensions = new Dimensions(20,20,5);
-Case theCase = new Case("220B", " dell", "240", dimensions);
-Monitor monitor = new Monitor("27 inch Beast" , " Apple", 27, new Resolution(2500, 1440));
-//        new Resolution(2500, 1440) that creates an instance of a class without using a variable
-Motherboard motherboard = new Motherboard("BJ-200", "Asus",4, 6,"v2.44" );
-        PC thePC = new PC(theCase,monitor, motherboard);
 
-        thePC.getMonitor().drawPixelAt(3,5,"green");
-        thePC.getMotherBoard().loadProgram("Apple IOSX");
-
-
-        thePC.getTheCase().pressPowerButton();
     }
 
 }
