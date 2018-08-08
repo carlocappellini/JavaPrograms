cbeb81bace32f1cd9b9b38609d20fdd1bba640cf
Bolimbo <carlo.cappellini@yahoo.com>
2018-08-08 12:43:14 -0500

    sorting method done

--- src/com/carlocappellini/Arrays.java
+++ src/com/carlocappellini/Arrays.java
@@ -18,7 +18,7 @@ public class Arrays {
         }
 
         for (int i = 0; i < array; i++) {
-            System.out.println("number are " + arr[i]);
+            System.out.println("number is " + arr[i]);
         }
         return arr;
 
@@ -34,16 +34,15 @@ public class Arrays {
         return array.length;
     }
 
-    private static int[] sortArray(int[] array) {
-        int[] arr = new int[array.length];
+    public static int[] sortArray(int[] array) {
+        int[] sortedArray = new int[array.length];
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("value is " + array[i]);
-            arr[i] = array[i];
+            sortedArray[i] = array[i];
         }
 
         boolean flag = true;
-        int[] sortedArray = new int[array.length];
         int temp;
 
 
@@ -51,15 +50,15 @@ public class Arrays {
             flag = false;
 
 
-            for (int i = 0; i < sortedArray.length - 1; i++) {
-
-                sortedArray[i] = array[i];
-
-                if (sortedArray[i + 1] < sortedArray[i]) {
+            for (int i=0; i<sortedArray.length-1; i++) {
+                if (sortedArray[i]<sortedArray[i+1]) {
                     temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[i + 1];
+                    System.out.println("temp " +temp);
+                    sortedArray[i] = sortedArray[i+1];
                     sortedArray[i + 1] = temp;
+                    System.out.println("temp 2 " +temp);
 
+                    System.out.println("sort " + sortedArray[i]);
                     flag = true;
                 }
 
@@ -77,12 +76,13 @@ public class Arrays {
 
         int[] ints = {8, 67, 1, 5, 2};
 
-        sortArray(ints);
-
-//    int [] q = getIntegers(5);
 
 
-        printArray(ints);
+    int [] q = getIntegers(5);
+
+int [] sorted = sortArray(q);
+
+        printArray(sorted)
     }
 }
 
