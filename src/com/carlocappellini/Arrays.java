d4b4b0d6787e154c471d85d024f60f9fe5871b06
Bolimbo <carlo.cappellini@yahoo.com>
2018-08-07 19:37:11 -0500

    Arrays

---
+++ src/com/carlocappellini/Arrays.java
@@ -0,0 +1,27 @@
+package com.carlocappellini;
+
+public class Arrays {
+
+    static int  [] arr = new int [10];
+    static int [] arrayToSort = new int[] {106,81,26,15,5};
+
+
+
+
+
+    public  static int array(int[] array){
+        for (int i = 0;i < array.length; i ++){
+    //            i`
+
+        }
+        return array.length;
+
+    }
+
+
+
+    public static void main(String[] args) {
+array(arrayToSort);
+    }
+}
+
