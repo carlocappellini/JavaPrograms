f4822e11419d48464b6df0eb4539da0df430f553
Bolimbo <carlo.cappellini@yahoo.com>
2018-08-29 21:48:33 -0500

    Inner Classes Car method

--- src/com/carlocappellini/GearBox.java
+++ src/com/carlocappellini/GearBox.java
@@ -6,36 +6,37 @@ public class GearBox {
     private int currentGear = 0;
     private int maxGears;
 
+    private boolean clutch;
+    ArrayList<Gear> gears;
 
-    public ArrayList<Gear> gears;
 
     public GearBox(int maxGears) {
         this.maxGears = maxGears;
         this.gears = new ArrayList<Gear>();
 
-
         for (int i = 0; i <= maxGears; i++) {
-            this.gears.add(new Gear(i, i * 5.3));
+            addGear(i, i * 5.3);
         }
     }
 
-    public boolean addGear(int newGear, double ratio) {
+    public void addGear(int newGear, double ratio) {
         if (newGear > 0 && newGear <= maxGears) {
             this.gears.add(new Gear(newGear, ratio));
-            return true;
         }
-        return false;
     }
-    public void changeGear(int newGear){
-        if (newGear > 0 && newGear <= this.gears.size()) {
+
+    public void changeGear(int newGear) {
+        if (newGear > 0 && newGear <= this.gears.size() && this.clutch) {
             this.currentGear = newGear;
+            clutchIn(false);
             System.out.println(newGear);
-        }else {
+        } else {
             System.out.println("grind");
         }
     }
-
-
+    public void clutchIn(boolean clutch){
+        this.clutch = clutch;
+    }
     public class Gear {
         private int gearNumber = 0;
         private double ratio;
@@ -44,8 +45,6 @@ public class GearBox {
             this.gearNumber = currentGear;
             this.ratio = ratio;
         }
-
-
 
 
     }
--- src/com/carlocappellini/Main.java
+++ src/com/carlocappellini/Main.java
@@ -526,72 +526,82 @@ public class Main {
 //        } else {
 //            System.out.println("playlist completed");
 //        }
-public static void main(String[] args) {
 
 
-Player carlo = new Player("Carlo",15);
+//////////////////////////////////////////////////////////////
+//
+//Player carlo = new Player("Carlo",15);
+//
+//    ISaveable werewolf = new Monster("Werewolf",20,10);
+//
+//        System.out.println(carlo.toString());
+//        saveObject(carlo);
+//
+//        carlo.setStrength(20);
+//        System.out.println(carlo);
+//        carlo.setWeapon("Mace");
+//        saveObject(carlo);
+//        System.out.println(carlo.toString());
+//        loadObject(carlo);
+//        System.out.println(carlo);
+//
+//        System.out.println(werewolf);
+//        saveObject(werewolf);
+//        //object casting
+//    System.out.println(((Monster)werewolf).getStrength());
+//
+//}
+//    public static ArrayList<String> readValues(){
+//        ArrayList<String> values = new ArrayList<String>();
+//
+//        Scanner scanner = new Scanner(System.in);
+//        boolean quit = false;
+//
+//        int index = 0 ;
+//        System.out.println("Choose\n " +
+//        "1 to enter a string\n"+
+//        "0 to quit");
+//
+//        while (!quit){
+//            System.out.println("Choose An Option");
+//            int choice = scanner.nextInt();
+//            scanner.nextLine();
+//            switch (choice){
+//
+//                case 0:
+//                    quit =true;
+//                    break;
+//
+//                case 1:
+//                    System.out.println("Enter a string");
+//                    String stringInput = scanner.nextLine();
+//                    values.add(index,stringInput);
+//                    index++;
+//                    break;
+//            }
+//        }
+//        return values;
+//    }
+//    public static void saveObject(ISaveable objectToSave){
+//        for (int i =0; i<objectToSave.write().size(); i++ ){
+//            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+//        }
+//
+//
+//    }
+//
+//    public static void loadObject(ISaveable objectToLoad){
+//        ArrayList<String> values = readValues();
+//        objectToLoad.read(values);
 
-    ISaveable werewolf = new Monster("Werewolf",20,10);
 
-        System.out.println(carlo.toString());
-        saveObject(carlo);
+    public static void main(String[] args) {
 
-        carlo.setStrength(20);
-        System.out.println(carlo);
-        carlo.setWeapon("Mace");
-        saveObject(carlo);
-        System.out.println(carlo.toString());
-        loadObject(carlo);
-        System.out.println(carlo);
-
-        System.out.println(werewolf);
-        saveObject(werewolf);
-        //object casting
-    System.out.println(((Monster)werewolf).getStrength());
-
-}
-    public static ArrayList<String> readValues(){
-        ArrayList<String> values = new ArrayList<String>();
-
-        Scanner scanner = new Scanner(System.in);
-        boolean quit = false;
-
-        int index = 0 ;
-        System.out.println("Choose\n " +
-        "1 to enter a string\n"+
-        "0 to quit");
-
-        while (!quit){
-            System.out.println("Choose An Option");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice){
-
-                case 0:
-                    quit =true;
-                    break;
-
-                case 1:
-                    System.out.println("Enter a string");
-                    String stringInput = scanner.nextLine();
-                    values.add(index,stringInput);
-                    index++;
-                    break;
-            }
-        }
-        return values;
-    }
-    public static void saveObject(ISaveable objectToSave){
-        for (int i =0; i<objectToSave.write().size(); i++ ){
-            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
-        }
+        GearBox toyota = new GearBox(8);
+        toyota.clutchIn(true);
+        toyota.changeGear(1);
 
 
-    }
-
-    public static void loadObject(ISaveable objectToLoad){
-        ArrayList<String> values = readValues();
-        objectToLoad.read(values);
     }
 }
 
