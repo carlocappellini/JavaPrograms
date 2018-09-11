c6163f6b4ed2717f57724afe9bc5dda913fcb473
Bolimbo <carlo.cappellini@yahoo.com>
2018-09-11 12:57:43 -0500

    created an ArrayList that is using generics for the Jugador object

--- src/com/carlocappellini/Team.java
+++ src/com/carlocappellini/Team.java
@@ -1,4 +1,24 @@
 package com.carlocappellini;
 
+import java.util.ArrayList;
+
 public class Team {
+    private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+
+private ArrayList<Jugador> members = new ArrayList<>();
+
+
+    public Team(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
